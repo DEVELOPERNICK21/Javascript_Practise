e755b6bc2a4d9e555233ae4a8ad0e43dcedfd5a9
nick <nikhilkubde21@gmail.com>
2021-05-24 02:13:43 +0530

    this is the first commit

---
+++ Var_datatypes.js
@@ -0,0 +1,17 @@
+// Variables are 
+    // var, let and const
+
+// Datatypes
+
+    var fullName = "Nick Kubde";
+
+    console.log(fullName);
+    
+    var checkIn = false;
+
+    console.log(checkIn);
+    
+    var intDatatype = 5;
+
+    console.log(intDatatype);
+
---
+++ signUP.js
@@ -0,0 +1,24 @@
+var uid = "fghjk741";
+var firstName = "Nick";
+var lastName = "Kubde";
+var email = "nikhilkubde21@gmail.com";
+var city = "Bhopal";
+var country = "India";
+var password = "password";
+var confirmPassword = "password";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+var isLoggedInFromFacebook = false;
+
+
+console.log(
+    `Your Unique ID is ${uid} 
+    Your First Name is ${firstName}
+    Your Last Name is ${lastName}
+    Your Email is ${email}
+    Your City is ${city}
+    Your Lovely Country is ${country}
+    uses password is ${password}
+    Loggin from google : ${courseCount}
+`
+);
