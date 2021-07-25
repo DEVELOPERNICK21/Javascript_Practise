2a5d826e9e1d46e80ff54fe84b83c80e30ee0103
NICK KUBDE <nikhilkubde21@gmail.com>
2021-07-26 01:29:06 +0530

    Learned about string in Javascript and template literals

---
+++ template_literlas.js
@@ -0,0 +1,7 @@
+var name = "Nick";
+var fullName = `${name} Developer`;
+
+console.log(fullName);
+console.log(fullName.toUpperCase());
+console.log(fullName.charCodeAt("3"));
+console.log(fullName.charAt('2'));
